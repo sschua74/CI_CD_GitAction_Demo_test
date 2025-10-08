e4397e91b990f277514e050a51a35066bcc7ac85
sschua74 <sschua74@yahoo.com.sg>
2025-10-08 19:27:23 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
     const product = {
-        name: 'Super Sneakers 2',
+        name: 'Super Sneakers',
         price: 79.99,
         description: 'Comfy, cool, and perfect for everyday adventures.'
     };
